6a3f73d17b5f4775ef755aac63ccab03685f8f59
thuan20132000 <thuan20132000@gmail.com>
2021-04-03 23:33:19 +0700

    Updated dictionary

--- dictionary/admin.py
+++ dictionary/admin.py
@@ -9,6 +9,7 @@ from .models import Vocabulary,Topic
 class AdminVocabulary(admin.ModelAdmin):
     search_fields  = ['name']
     list_filter = ('topic','status',)
+    list_display = ['name','created_at','topic']
 
 
 
--- dictionary/models.py
+++ dictionary/models.py
@@ -46,18 +46,19 @@ class Vocabulary(models.Model):
         ('ielts', 'IELTS'),
         ('toeic', 'TOEIC'),
         ('toefl', 'TOEFL'),
-        ('common','COMMON'),
+        ('common', 'COMMON'),
     )
-    ID = models.UUIDField(default=uuid.uuid4,editable=False,primary_key=True)
-    name = models.CharField(max_length=100,null=True)
-    word_type = models.CharField(max_length=100,null=True)
-    phon_us = models.CharField(max_length=100,null=True)
-    phon_uk = models.CharField(max_length=100,null=True)
-    sound_us = models.CharField(max_length=255,null=True)
-    sound_uk = models.CharField(max_length=255,null=True)
+    ID = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    name = models.CharField(max_length=100, null=True)
+    word_type = models.CharField(max_length=100, null=True)
+    phon_us = models.CharField(max_length=100, null=True)
+    phon_uk = models.CharField(max_length=100, null=True)
+    sound_us = models.FileField(upload_to='audio/', blank=True)
+    sound_uk = models.FileField(upload_to='audio/', blank=True)
     definitions = models.JSONField(null=True, blank=True)
 
-    certification_field =  models.CharField(choices=CERTIFICATION_CHOICES,max_length=10,default="common")
+    certification_field = models.CharField(
+        choices=CERTIFICATION_CHOICES, max_length=10, default="common")
 
     topic = models.ForeignKey(
         Topic,
@@ -66,11 +67,14 @@ class Vocabulary(models.Model):
         null=True
     )
 
-
     status = models.TextField(
         max_length=16, choices=STATUS_CHOICES, default='published')
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self,):
         return f"Name: {self.name}"
